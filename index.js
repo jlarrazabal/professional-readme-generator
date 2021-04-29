03268835077ff47633bbe94d1389585048fafc29
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-04-28 23:54:00 -0400

    Adding function that creates the NEWREADME.md file and the template.
    Adding function that creates the NEWREADME.md file and the template.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -23,6 +24,16 @@ const questions = [{
     message: "How can you application be used?"
   },
   {
+    type: 'input',
+    name: 'appContributions',
+    message: 'How can others contribute to your application?'
+  },
+  {
+    type: 'input',
+    name: 'appTest',
+    message: 'How can your application be tested?'
+  },
+  {
     type: 'list',
     name: 'appLicense',
     message: 'Under what license is your application covered?',
@@ -40,18 +51,49 @@ const questions = [{
   },
 ];
 
-let newAnswer = inquirer.prompt(questions).then((answers) => {
-  console.log(JSON.stringify(answers, null, '  '));
-  return answers;
-});
-
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {}
+function writeToFile(template) {
+  fs.writeFile("NEWREADME.MD", template, function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+});
+
+
+}
 
 // TODO: Create a function to initialize app
 
-function init() {}
+function init() {
+  let newAnswer = inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+    let template =
+    `# ${answers.appName}\n
+    ## ${answers.appDescription}\n
+    ## Table of content\n
+    - [Installation] (#installation)\n
+    - [Usage] (#usage)\n
+    - [Contributions-Guidelines] (#contributions-guidelines)\n
+    - [Tests] (#tests)\n
+    - [License] (#license)\n
+    - [Questions] (#questions)\n
+    ## Installation\n
+    ${answers.appInstallation}\n
+    ## Usage\n
+    ${answers.appUsage}\n
+    ## Constributions-Guidelines\n
+    ${answers.appContributions}\n
+    ## Tests\n
+    ${answers.appTest}\n
+    ## License\n
+    ${answers.appLicense}\n
+    ## Questions\n
+    - For mor information please visit https://github.com/${userAnswers.appGitHubUserName}\n
+    - In case of questions feel free to contact me at ${userAnswers.appUserEmail}\n
+    `;
+    writeToFile(template);
+  });
+}
 
 // Function call to initialize app
 init();
